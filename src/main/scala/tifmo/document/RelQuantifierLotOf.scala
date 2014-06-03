5959d97b5a2b01048e271a3c8a5e1422d5680ba5
Tom Yubing Dong <tom.tung.dyb@gmail.com>
2014-06-03 16:58:20 +0900

    Make "many" & "lots of" non-monotone in 1st arg
    This matches the interpretation of FraCaS people.
    
    Side note: now all generalized quantifiers are implemented as Relations.
    We achieve a 90% accuracy on the first section of FraCaS corpus. The
    errors are: 030, 031, 032, 046, 048, 049, 058, 069. Among them, 049 and
    058 are due to named-entity recognition problems, while 069 is due to
    the lack of knowledge of “can travel freely within” being equivalent to
    “have the right to live in”. The other 5 errors are all due to failure
    of handling interation between quantifiers.
    
    
    Former-commit-id: 218b420a14904b29916a665eb99d89e4b948d201

--- src/main/scala/tifmo/document/RelQuantifierLotOf.scala
+++ src/main/scala/tifmo/document/RelQuantifierLotOf.scala
@@ -4,7 +4,9 @@ import tifmo.dcstree.Relation
 
 case object RelQuantifierLotOf
   extends Relation
-  with RelLeftUpEntailing
+  // The following line is commented out to make this quantifier non-monotone in the first argument
+  // This matches the interpretation of FraCaS people
+  //  with RelLeftUpEntailing
   with RelRightUpEntailing
   with RelWithNonEmptyIntersection
   with RelConservative
